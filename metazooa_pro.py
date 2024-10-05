73395040874fbd6eaddc91a69dcb6005a0a6e8a6
etd530 <etd530@gmail.com>
2024-10-05 14:26:30 +0200

    Updated game to be in functions and be able to run on subsets of the tree of life (e.g. Papilionoidea)

--- metazooa_pro.py
+++ metazooa_pro.py
@@ -3,6 +3,7 @@
 #### LIBS ####
 import taxopy
 import random as rd
+import os
 
 #### FUNS ####
 def name2taxid(taxdb):
@@ -13,7 +14,7 @@ def name2taxid(taxdb):
 		taxdb: A taxdb object from the taxopy library, contanining the NCBI taxonomy database or an equivalently formatted custom database.
 
 	Returns:
-		A string containing the proposed taxon's name.
+		The proposed taxon's taxid.
 
 	"""
 	while 1:
@@ -26,39 +27,77 @@ def name2taxid(taxdb):
 			del proposed_taxname
 			print("That is not a valid species name. Please revise spelling an try again:")
 
-
-def find_max_taxid(taxdb, delnodes):
+def find_max_taxid(nodes_file):
 	"""
-	Given a taxdb object and the path to a file of deleted node numbers, find the maximum valid taxid.
+	Givena a taxdb file, find the maximum taxid.
 
 	Arguments:
-		taxdb: A taxdb object from the taxopy library, contanining the NCBI taxonomy database or an equivalently formatted custom database.
-		delnodes: The name of a file containing the deleted nodes from the NCBI taxonomy database.
+		nodes_file: The name of the file containing the taxid nodes.
 
 	Returns:
-		The highest valid taxid in the database.
+		The hights valid taxid in the file.
 	"""
-	# Read delnodes into a list
-	delnodes_list = []
-	with open(delnodes, 'r') as fh:
-		for line in fh:
-			delnodes_list.append(int(line.strip().split("\t")[0]))
 
-	# Get maxmimum taxid taking into account the deleted taxids
-	i = 1 # we start at one which is the taxid for the root of the tree of life (i.e. the LUCA)
+	command = "cut -f1 %s | tail -n1" % nodes_file
+	max_taxid = int(os.popen(command).read().strip())
+	return(max_taxid)
+
+def generate_mystery_taxon(max_taxid, limit_taxid, taxdb):
+	"""
+	Given the maximum taxid possible and the taxid of which to limit the search, generate a random taxid that is a descendant of the limiting taxid (e.g. a species within a family).
+
+	Arguments:
+		max_taxid: integer indicating the maximum allowed taxid in the database.
+		limit_taxid: the taxid which limits the search.
+		taxdb: the taxonomy database form which to generate the taxid.
+	
+	Returns:
+		A randomly generated taxon entry that is a descendant of the 'limit_taxon' taxid.
+	"""
+	limit_taxon = taxopy.Taxon(limit_taxid, taxdb)
 	while 1:
-		print(i)
 		try:
-			max_taxon = taxopy.Taxon(i, taxdb)
-			i +=1
-		except taxopy.exceptions.TaxidError:
-			if i in delnodes_list:
-				i += 1
-				continue
-			else:
-				max_taxid = max_taxon.taxid
-				print("Maximum taxid is: %s" %str(max_taxid))
-				return(max_taxid)
+			mystery_taxid = rd.randint(1, max_taxid)
+			mystery_taxon = taxopy.Taxon(mystery_taxid, taxdb) # Need to change, BUT need to provide robustness against missing taxids that could appear
+			assert mystery_taxon.rank == 'species'
+			assert taxopy.find_lca([mystery_taxon, limit_taxon], taxdb).taxid == limit_taxon.taxid
+			return mystery_taxon
+		except:
+			pass
+
+def play_metazooa_pro(mystery_taxon, taxdb, attempts = 10):
+	"""
+	Given a mystery taxon, a taxonomy databse, and a number of attempts, let the user guess the correct species as many times as attempts.
+
+	Arguments:
+		mystery_taxon: a Taxopy.Taxon object of the species to guess.
+		taxdb: the Taxopy.taxdb object containing the taxonomy database.
+		attempts: integer indicating the number of rounds the user can play.
+
+	Returns:
+		None.
+	"""
+	# Ask for user input of species name
+	print("Done! Now you can start guessing the species:")
+	while attempts:
+		proposed_taxid = name2taxid(taxdb)
+
+		# Transform proposed taxid to taxon
+		proposed_taxon = taxopy.Taxon(proposed_taxid, taxdb)
+
+		# Find LCA of propsed taxon and mystery taxon
+		lca_taxon = taxopy.find_lca([mystery_taxon, proposed_taxon], taxdb)
+
+		# If LCA is the same as mystery taxon, they found the taxon
+		if lca_taxon.taxid == mystery_taxon.taxid:
+			print("Congratulations! You found the mystery species: %s" % mystery_taxon.name)
+			exit("Thanks for playing!\n")
+		else:
+			attempts -=1
+			print("That is not the mystery species, but both your proposed species and the mystery species are members of %s." % lca_taxon.name)
+			print("Remaining attempts: %s" % str(attempts))
+
+	print("You lose! The mystery species was %s" % mystery_taxon.name)
 
 if __name__ == '__main__':
 	#### VARS ####
@@ -75,46 +114,14 @@ if __name__ == '__main__':
 
 	# Select clade to limit the game to
 	print("The game with all taxa can be absurdly hard. Do you want to limit the game to some set of species? If so, enter the name of the group to limit to:") # HAVE TO FIND HOW TO ENTER ROOT IN CASE SOMEONE IS CRAZY
-	limit_taxon = name2taxid(taxdb)
+	limit_taxid = name2taxid(taxdb)
 
-	# Find max taxid available
-
-
-	# For now we skip because of taxdump meaning taxids are not consecutive and I was lazy to find the taxdump file now
-	max_taxid = 1000000 # placeholder, this is NOT the max taxid at all
-
+	# Find max taxid in the database
+	max_taxid = find_max_taxid("nodes.dmp")
 
 	# Generate random taxid for the mystery species, make sure it is a species and not a genus or whatever
 	print("Please wait while we decide on a mystery species for you to find...")
-	while 1:
-		try:
-			mystery_taxid = rd.randint(1, max_taxid)
-			mystery_taxon = taxopy.Taxon(mystery_taxid, taxdb) # Need to change, BUT need to provide robustness against missing taxids that could appear
-			assert mystery_taxon.rank == 'species'
-			break
-		except:
-			pass
+	mystery_taxon = generate_mystery_taxon(max_taxid, limit_taxid, taxdb)
 
-	# Ask for user input of species name
-	print("Done! Now you can start guessing the species:")
-	attempts = 10
-	while attempts:
-		proposed_taxid = name2taxid(taxdb)
-
-		# Tranform proposed taxid to taxon
-		proposed_taxon = taxopy.Taxon(proposed_taxid, taxdb)
-
-
-		# Find LCA of propsed taxon and mystery taxon
-		lca_taxon = taxopy.find_lca([mystery_taxon, proposed_taxon], taxdb)
-
-		# If LCA is the same as mystery taxon, they found the taxon
-		if lca_taxon.taxid == mystery_taxid:
-			print("Congratulations! You found the mystery species: %s" % mystery_taxon.name)
-			exit("Thanks for playing!\n")
-		else:
-			attempts -=1
-			print("That is not the mystery species, but both your proposed species and the mystery species are members of %s." % lca_taxon.name)
-			print("Remaining attempts: %s" % str(attempts))
-
-	print("You lose! The mystery species was %s" % mystery_taxon.name)
+	# Play the game!
+	play_metazooa_pro(mystery_taxon, taxdb)
--- test_metazooa_pro.py
+++ test_metazooa_pro.py
@@ -8,10 +8,14 @@ taxdb = mt.taxopy.TaxDb(nodes_dmp = "test_nodes.dmp", names_dmp = "test_names.dm
 #### Tests for find_max_taxid ####
 # Test that it returns the correct last node, skipping deleted ones
 def test_find_max_taxid():
-	assert mt.find_max_taxid(taxdb, "test_delnodes.dmp") == 10
+	assert mt.find_max_taxid("test_nodes.dmp") == 10
 
 #### Tests for name2taxid ####
 # Tets that it returns the correct taxid from a species name
 def test_name2taxid_species_name(monkeypatch):
 	with mock.patch('builtins.input', return_value = 'Azorhizobium'):
 		assert mt.name2taxid(taxdb) == 6
+
+#### Tests for generate_mystery_taxon ####
+def test_generate_mystery_taxon(monkeypatch):
+	assert mt.generate_mystery_taxon(max_taxid = 10, limit_taxid = 6, taxdb = taxdb).name == 'Azorhizobium caulinodans'
